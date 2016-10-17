4cf9fc48eba28a98ea949617d4182a14b7cc8a8f
CYC666 <13705038428@163.com>
2016-10-17 16:42:07 +0800

    解决了41个错误的问题，记录在了印象笔记。 百度地图集成的时候，步骤有错，真坑。

---
+++ CMap/CMap/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  CMap
+//
+//  Created by mac on 16/10/17.
+//  Copyright © 2016年 CYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BDheader.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@property (strong, nonatomic) BMKMapManager *mapManager;
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
